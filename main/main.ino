68a013b2821146e32fb5891fc9cb403c177d1af6
mattestanka <53266220+mattestanka@users.noreply.github.com>
2024-12-18 10:15:48 +0100

    Update main.ino
    Init fs

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,27 @@
 #include "WiFiSetup.h"
 #include "DeejControl.h"
+#include <SPIFFS.h>
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 17, 5);
 
 void setup() {
     Serial.begin(115200);
     u8g2.begin();
+
+     // Initialize SPIFFS
+    if (!SPIFFS.begin(true)) { // `true` formats the file system if it's not initialized
+        Serial.println("SPIFFS Mount Failed");
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB08_tr);
+        u8g2.drawStr(0, 15, "SPIFFS Mount Failed");
+        u8g2.drawStr(0, 35, "Restart Required");
+        u8g2.sendBuffer();
+        while (true) { // Halt execution if the file system fails to mount
+            delay(1000);
+        }
+    } else {
+        Serial.println("SPIFFS Mounted Successfully");
+    }
 
     // Initialize WiFi setup
     initWiFiSetup();
